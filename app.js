27cdc52cd68a7c67677df523f2e215ae641a3d44
Tomgtech <tom.gendall@yahoo.com>
2024-01-17 13:59:55 +0000

    forms

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  console.log(formData);
+});
